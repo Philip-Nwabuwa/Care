32caf4b5826da02db0ed51fc679613468c727760
Philip John <philipnwabuwa@gmail.com>
2023-05-18 10:46:08 +0100

    fixed login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 import { useState } from "react";
 import { loginWithEmailAndPassword } from "@/lib/firebase";
+import { createUser } from "@/lib/firebase";
 import React from "react";
 import { useToggle, upperFirst } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
@@ -34,6 +36,18 @@ function AuthenticationForm(props: PaperProps) {
       console.log(error);
     }
   };
+  const Signup = () => {
+    const [SignupMail, setSignupMail] = useState("");
+    const [SignupPassword, setSignupPassword] = useState("");
+  
+    const handleSignup = async (e: { preventDefault: () => void }) => {
+      e.preventDefault();
+      try {
+        await createUser(email, password);
+      } catch (error) {
+        console.log(error);
+      }
+    };
   const [type, toggle] = useToggle(["login", "register"]);
   const form = useForm({
     initialValues: {
@@ -65,50 +79,63 @@ function AuthenticationForm(props: PaperProps) {
 
       <Divider label="Or continue with email" labelPosition="center" my="lg" />
 
-      <form
-        onSubmit={form.onSubmit(() => {
-          handleLogin;
-        })}
-      >
+      <form onSubmit={handleLogin}>
         <Stack>
-          {type === "register" && (
-            <TextInput
-              label="Name"
-              placeholder="Your name"
-              value={form.values.name}
-              onChange={(event) =>
-                form.setFieldValue("name", event.currentTarget.value)
-              }
-              radius="md"
-            />
+          {type === "login" && (
+            <>
+              <TextInput
+                required
+                type="email"
+                label="Email"
+                placeholder="hello@mantine.dev"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={form.errors.email && "Invalid email"}
+                radius="md"
+              />
+              <PasswordInput
+                required
+                label="Password"
+                type="password"
+                placeholder="Your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                error={
+                  form.errors.password &&
+                  "Password should include at least 6 characters"
+                }
+                radius="md"
+              />
+            </>
           )}
 
-          <TextInput
-            required
-            label="Email"
-            placeholder="hello@gmail.com"
-            value={form.values.email}
-            onChange={(event) =>
-              form.setFieldValue("email", event.currentTarget.value)
-            }
-            error={form.errors.email && "Invalid email"}
-            radius="md"
-          />
-
-          <PasswordInput
-            required
-            label="Password"
-            placeholder="Your password"
-            value={form.values.password}
-            onChange={(event) =>
-              form.setFieldValue("password", event.currentTarget.value)
-            }
-            error={
-              form.errors.password &&
-              "Password should include at least 6 characters"
-            }
-            radius="md"
-          />
+          {type === "register" && (
+            <>
+              <TextInput
+                required
+                type="email"
+                label="Email"
+                placeholder="hello@mantine.dev"
+                value={SignupMail}
+                onChange={(e) => setSignupMail(e.target.value)}
+                error={form.errors.email && "Invalid email"}
+                radius="md"
+              />
+              <PasswordInput
+                required
+                label="Password"
+                type="password"
+                placeholder="Your password"
+                value={SignupPassword}
+                onChange={(e) => setSignupPassword(e.target.value)}
+                error={
+                  form.errors.password &&
+                  "Password should include at least 6 characters"
+                }
+                radius="md"
+              />
+            </>
+          )}
 
           {type === "register" && (
             <Checkbox
@@ -133,9 +160,16 @@ function AuthenticationForm(props: PaperProps) {
               ? "Already have an account? Login"
               : "Don't have an account? Register"}
           </Anchor>
-          <Button type="submit" radius="xl">
-            {upperFirst(type)}
-          </Button>
+          {type === "login" && (
+            <Button className="bg-[#228be6]" type="submit" radius="xl">
+              {upperFirst(type)}
+            </Button>
+          )}
+          {type === "register" && (
+            <Button className="bg-[#228be6]" type="submit" radius="xl">
+              {upperFirst(type)}
+            </Button>
+          )}
         </Group>
       </form>
     </Paper>
